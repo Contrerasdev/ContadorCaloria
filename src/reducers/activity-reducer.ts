6f85d518266d1cbef5af6fcf7eee8d43665f7115
Walter Contreras <w_contreras@outlook.com>
2024-06-03 14:53:06 -0600

    Se ordeno el codigo, en la parte del reducer

--- src/reducers/activity-reducer.ts
+++ src/reducers/activity-reducer.ts
@@ -1,59 +1,61 @@
 import { Activity } from "../Types";
 
-// === Almacenar las acciones de las actividades ===
-export type ActivityActions =
-    {type:"save-activity"; payload: { newActivity: Activity }} |
-    {type:"set-activeId"; payload: { id: Activity['id'] } } |
-    {type:"delete-activity"; payload:{id: Activity['id']} } |
-    {type:"reset-app"}
-
-// === State Activity ===
+//1-) === State Activity ===
 export type ActivityState = {
   activities: Activity[];
-  activeId : Activity['id'];
+  activeId: Activity['id'];
 };
 // === State initial ===
 export const initialState: ActivityState = {
   activities: [],
-  activeId : ''
+  activeId: ''
 };
-// === Reducer ===
+
+// 2) === Almacenar las acciones de las actividades ===
+export type ActivityActions =
+  { type: "save-activity"; payload: { newActivity: Activity } } |
+  { type: "set-activeId"; payload: { id: Activity['id'] } } |
+  { type: "delete-activity"; payload: { id: Activity['id'] } } |
+  { type: "reset-app" }
+
+
+// 3-) === Reducer ===
 export const activityReducer = (state: ActivityState = initialState, action: ActivityActions) => {
   // == Ejecutar la accion save-actvivity ===  
   if (action.type === "save-activity") {
     // Manejar la logica para actualizar el state    
-    let updateActividad:Activity[] = [] //Generar un state vacio
-    if(state.activeId){ 
-        updateActividad = state.activities.map(activ => activ.id === state.activeId ? action.payload.newActivity : activ);
-    }else{
-        updateActividad=[...state.activities, action.payload.newActivity]
+    let updateActividad: Activity[] = [] //Generar un state vacio
+    if (state.activeId) {
+      updateActividad = state.activities.map(activ => activ.id === state.activeId ? action.payload.newActivity : activ);
+    } else {
+      updateActividad = [...state.activities, action.payload.newActivity]
     }
     return {
       ...state,
-      activities:updateActividad,
-      activeId:'' //Reiniciar al guardar registro
+      activities: updateActividad,
+      activeId: '' //Reiniciar al guardar registro
     };
   }
   // == Ejecutar la accion set-activedid ===
-  if(action.type === "set-activeId"){
-    return{
-        ...state,
-        activeId:action.payload.id               
+  if (action.type === "set-activeId") {
+    return {
+      ...state,
+      activeId: action.payload.id
     }
-  }  
+  }
   // === Delete una actividad ==
-  if(action.type === "delete-activity"){   
-        return{
-            ...state,
-           activities:state.activities.filter((actividad) => actividad.id !== action.payload.id )
-            
-        }
+  if (action.type === "delete-activity") {
+    return {
+      ...state,
+      activities: state.activities.filter((actividad) => actividad.id !== action.payload.id)
+
+    }
   }
   // === Reset app ==
-  if(action.type="reset-app"){
-    return{
-      activities:[],
-      activeId:''
+  if (action.type = "reset-app") {
+    return {
+      activities: [],
+      activeId: ''
     }
   }
 
